fc504b71437d9b27aea0cc4e7f1fd9a1b4d19589
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2023-05-23 11:18:44 -0300

    server e web

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('listening on localhost:3333')
+  })
